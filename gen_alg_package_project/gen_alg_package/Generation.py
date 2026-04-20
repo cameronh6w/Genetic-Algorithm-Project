4cc27369f4cf19f067fc203904ffa1d280e3b345
cameronh6w <cgch6w@umsystem.edu>
2026-04-20 13:11:22 -0500

    moved my  random schedule generator out of main

--- gen_alg_package_project/gen_alg_package/Generation.py
+++ gen_alg_package_project/gen_alg_package/Generation.py
@@ -2,6 +2,7 @@
 #Okay, being specific. A generation is gonna use SOME data structure to hold N >= 250 Schedules, and best_score, worst_score, and avg_score variables for the fitness scores. On creation, we're gonna fill that data structure with the given amount of schedules randomly created by our random schedule creation function.
 
 import Schedule
+import Functions
 
 class Generation:
     def __init__(self, gen_size: int):
@@ -15,3 +16,7 @@ class Generation:
         for i in range(0, gen_size):
             #This is where we'll add the randomized schedule creation...TODO: IF WE HAD SOME! (not angry, this is a fairlyoddparents reference)
             self.population.append(ScheduleGenerator.new()) #or some shit
+
+            #implementing  what I did would look something like this - cameron
+            self.population.append(Functions.create_random_Schedule()) 
+            
